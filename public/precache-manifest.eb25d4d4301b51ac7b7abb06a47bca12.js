c05225e207b583a84c49dbc5b7695697c607317d
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2019-02-19 20:26:54 -0330

    maybe last

---
+++ public/precache-manifest.eb25d4d4301b51ac7b7abb06a47bca12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "173eac36370034efdbe6",
+    "url": "/static/css/main.91b0718d.chunk.css"
+  },
+  {
+    "revision": "173eac36370034efdbe6",
+    "url": "/static/js/main.173eac36.chunk.js"
+  },
+  {
+    "revision": "7a7826815ea4cb517b17",
+    "url": "/static/js/1.7a782681.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e1a8d6379bb048bdb17628b24991fda",
+    "url": "/index.html"
+  }
+];
