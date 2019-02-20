9a75e1b62b9c9a6dfb8a77af49271acc7616ed5e
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2019-02-19 20:55:02 -0330

    maybe fixed

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const typeDefs = require('./schema');
 const { ApolloServer } = require('apollo-server');
 const QuestionAPI = require('./datasources/question');
 const resolvers = require('./resolvers');
+const express = require('express');
+
+const app = express();
 
 const server = new ApolloServer({
   cors: true,
@@ -10,6 +13,12 @@ const server = new ApolloServer({
   dataSources: () => ({
     questionAPI: new QuestionAPI()
   })
+});
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
