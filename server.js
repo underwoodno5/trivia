b49a035af0a3417d4b8653d96dc31b5054bbe835
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2019-02-20 11:52:21 -0330

    static folder

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const QuestionAPI = require('./datasources/question');
 const resolvers = require('./resolvers');
 const express = require('express');
 
-const app = express();
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'));
+  server.use(express.static('public'));
 }
 
-app.get('*', (req, res) => {
+server.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
