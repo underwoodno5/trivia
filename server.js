f98ef7babcaffaa8f70c7a86bb216b522a2d8889
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2019-02-20 11:54:28 -0330

    static folder

--- server.js
+++ server.js
@@ -4,24 +4,12 @@ const QuestionAPI = require('./datasources/question');
 const resolvers = require('./resolvers');
 const express = require('express');
 
+const app = express();
+
 if (process.env.NODE_ENV === 'production') {
-  server.use(express.static('public'));
+  app.use(express.static('client/build'));
 }
 
-server.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-const server = new ApolloServer({
-  cors: true,
-  introspection: true,
-  typeDefs,
-  resolvers,
-  dataSources: () => ({
-    questionAPI: new QuestionAPI()
-  })
-});
-
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
 });
