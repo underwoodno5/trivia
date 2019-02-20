b4a888c084a0ce8ef5729345049f0156ea5f1fe0
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2019-02-20 11:48:25 -0330

    static folder

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public'));
+}
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
